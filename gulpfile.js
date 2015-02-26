6aa22ddf2aa535b5f2da54b92bce47c71f99b227
Andrew Zey <andrew@andrewzey.com>
2015-02-26 08:57:06 -0800

    add error handling to gulp lint task

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,9 @@ var gulp       = require('gulp');
 var jshint     = require('gulp-jshint');
 var stylish    = require('jshint-stylish');
 var react      = require('gulp-react');
-var gulpJest   = require('gulp-jest');
-// var istanbul   = require('gulp-istanbul'); //TODO: enable once Jest works with Istanbul
-// var coveralls  = require('gulp-coveralls'); //TODO: enable once Jest works with Istanbul
+var mocha      = require('gulp-mocha');
+var istanbul   = require('gulp-istanbul');
+var coveralls  = require('gulp-coveralls');
 var uglify     = require('gulp-uglify');
 var rename     = require('gulp-rename');
 var streamify  = require('gulp-streamify');
@@ -19,48 +19,40 @@ var reactify   = require('reactify');
 // ==================================
 gulp.task('lint', function () {
   gulp.src(['./!(coverage|node_modules|bower_components)/**/!(build)/*.{js,jsx}'])
-    .pipe( react({harmony: true}) )
+    .pipe( react({ harmony: true }) )
+    .on('error', console.log.bind(console))
     .pipe( jshint() )
     .pipe( jshint.reporter( stylish ) );
   });
 
-// Run Jest Tests
-// ==================================
-gulp.task('jest', function () {
-  return gulp.src(['./client'])
-  .pipe(gulpJest({
-    unmockedModulePathPatterns: ['node_modules/react'],
-    // testDirectoryName: "__tests__", //__tests_ is the default
-    testPathIgnorePatterns: ['node_modules'],
-    moduleFileExtensions: [
-      'js',
-      'jsx',
-      'json'
-    ]
-  }));
-});
 
-// TODO: once Jest works with Istanbul, setup code coverage
 // Run Tests and Generate Coverage Data
 // ====================================
-// gulp.task('test', function (cb) {
-//   gulp.src(['requestHandler.js'])
-//     .pipe(istanbul()) // Instrument files
-//     .on('finish', function () {
-//       gulp.src(['spec.js'])
-//         .pipe(mocha()) // Run Tests
-//         .pipe(istanbul.writeReports()) // Creating the reports after tests runned
-//         .on('end', cb);
-//     });
-// });
+gulp.task('test', function (cb) {
+  gulp.src(['/client/app/components/app/app.component.jsx'])
+    .pipe( react({harmony: true}) )
+    .pipe(istanbul()) // Instrument files
+    .on('finish', function () {
+      gulp.src(['/client/app/components/app/app.component.tests.jsx'])
+        .pipe(mocha()) // Run Tests
+        .pipe(istanbul.writeReports()) // Creating the reports after tests runned
+        .on('end', cb);
+    });
+});
 
-// TODO: once Jest works with Istanbul, setup code coverage
+gulp.task('mocha', function(){
+  return gulp.src(['tests.jsx'])
+      .pipe( react({ harmony: true }) )
+      .pipe( mocha() )
+});
+
+
 // Send Coverage Data to Coveralls
 // ====================================
-// gulp.task('coveralls', function(){
-//   return gulp.src('./coverage/lcov.info')
-//   .pipe(coveralls());
-// });
+gulp.task('coveralls', function(){
+  return gulp.src('./coverage/lcov.info')
+  .pipe(coveralls());
+});
 
 
 // Watch & Build Client JS Bundle
